93a59f4bb86504aae361e9717c465063b32200ba
IGD2017 <tun.onizuka@gmail.com>
2017-08-24 11:56:49 +0700

    setup Title on Toolbar OK

--- app/src/main/java/phansa/phaiboon/showtun/fragment/SignUpFragment.java
+++ app/src/main/java/phansa/phaiboon/showtun/fragment/SignUpFragment.java
@@ -3,10 +3,12 @@ package phansa.phaiboon.showtun.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import phansa.phaiboon.showtun.MainActivity;
 import phansa.phaiboon.showtun.R;
 
 /**
@@ -20,5 +22,35 @@ public class SignUpFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return view;
+    } // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Create ToolBar
+        createToolBar();
+
+
+    }// onActivityCreate
+
+    private void createToolBar() {
+        //SetUp Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolBarsignUp);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+
+        //setup Navagation Icon
+        toolbar.setNavigationIcon(R.mipmap.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+            }
+        });
+
+
     }
 }  //Main Class
