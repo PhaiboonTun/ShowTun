c16c2957eca2647e76189fa7314e8e292c372a5b
IGD2017 <tun.onizuka@gmail.com>
2017-08-28 11:30:47 +0700

    Read JSON For Ebook TABLE OK

--- app/src/main/java/phansa/phaiboon/showtun/fragment/ReadEbookFragment.java
+++ app/src/main/java/phansa/phaiboon/showtun/fragment/ReadEbookFragment.java
@@ -3,11 +3,15 @@ package phansa.phaiboon.showtun.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import phansa.phaiboon.showtun.R;
+import phansa.phaiboon.showtun.manager.GetAllData;
+import phansa.phaiboon.showtun.manager.Myconstant;
 
 /**
  * Created by Samsung on 25/8/2560.
@@ -17,8 +21,39 @@ public class ReadEbookFragment extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_read_ebook, container, false);
         return view;
-    }
+    }// OnCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Cerate ListView
+        cerateListView();
+
+    } //onActivityCreated
+
+    private void cerateListView() {
+        ListView listView = getView().findViewById(R.id.livEbook);
+        String tag = "28AugV1";
+        Myconstant myconstant = new Myconstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myconstant.getUrlEbookString());
+            String strJSON = getAllData.get();
+            Log.d(tag, "JSON ==>" + strJSON);
+
+        } catch (Exception e) {
+            Log.d(tag, "e crateListView ==> " + e.toString());
+
+        }
+
+    } // ceateListView
+
 } //Main Class
---
+++ app/src/main/java/phansa/phaiboon/showtun/manager/Myconstant.java
@@ -0,0 +1,13 @@
+package phansa.phaiboon.showtun.manager;
+
+/**
+ * Created by Samsung on 28/8/2560.
+ */
+
+public class Myconstant {
+    private String urlEbookString = "http://androidthai.in.th/Tun/getAllUserEbookTun.php";
+
+    public String getUrlEbookString() {
+        return urlEbookString;
+    }
+} //Main Calss
